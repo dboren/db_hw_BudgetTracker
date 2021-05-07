c4904e5d57517484d073a98f7c324fccf7152d7e
dboren <danielboren@att.net>
2021-05-06 20:40:29 -0500

    added onupgradeneeded function to db.js

--- public/db.js
+++ public/db.js
@@ -2,3 +2,18 @@ let db;
 let budgetVersion;
 
 const request = indexedDB.open('BudgetTrackerDB', budgetVersion || 1);
+
+request.onupgradeneeded = function (event) {
+    console.log('Upgrade needed in IndexedDB');
+
+    const { oldVersion } = event;
+    const newVersion = event.newVersion || db.version;
+
+    console.log(`DB updated from version ${oldVersion} to version ${newVersion}`);
+
+    db = event.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetTrackerStore', { autoIncrement: true });
+    }
+};
