8c7c371c77eed27e7b8afd5837f2a2e68af25b0e
dboren <danielboren@att.net>
2021-05-07 20:37:50 -0500

    added install event listener to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,12 @@ const FILES_TO_CACHE = [
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
