49877f901ed0185be7dc784a0bb9590afd673d51
dboren <danielboren@att.net>
2021-05-07 21:58:36 -0500

    added fetch event listener to service-worker. Still need to debug; app still runs but not getting offline functionality and something in db is logging as undefined

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,14 +12,6 @@ const FILES_TO_CACHE = [
 const STATIC_CACHE = 'static-cache-v2';
 const DATA_CACHE = 'data-cache-v1';
 
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches
-//             .open(PRECACHE)
-//             .then((cache) => cache.addAll(FILES_TO_CACHE))
-//             .then(self.skipWaiting())
-//     );
-// });
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
@@ -34,25 +26,6 @@ self.addEventListener('install', function (event) {
     self.skipWaiting();
 });
 
-// self.addEventListener('activate', (event) => {
-//     const currentCaches = [PRECACHE, RUNTIME];
-//     event.waitUntil(
-//         caches
-//             .keys()
-//             .then((cacheNames) => {
-//                 return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-//             })
-//             .then((cachesToDelete) => {
-//                 return Promise.all(
-//                     cachesToDelete.map((cacheToDelete) => {
-//                         return caches.delete(cacheToDelete);
-//                     })
-
-//                 );
-//             })
-//             .then(() => self.clients.claim())
-//     );       
-// });
 
 self.addEventListener('activate', function(event) {
     event.waitUntil(
@@ -72,32 +45,32 @@ self.addEventListener('activate', function(event) {
 });
 
 
-// self.addEventListener('fetch', function(event) {
-//     if (event.request.url.includes('/api/')) {
-//         event.respondWith(
-//             caches.open(DATA_CACHE_NAME).then(cache => {
-//                 return fetch(event.request)
-//                     .then(res => {
-//                         if (res.status === 200) {
-//                             cache.put(event.request.url, res.clone());
-//                         }
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('/api/')) {
+        event.respondWith(
+            caches.open(DATA_CACHE).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
 
-//                         return res;
-//                     })
-//                     .catch(err => {
-//                         return cache.match(event.request);
-//                     });
-//             }).catch(err => console.log(err))      
-//         );
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
 
-//         return;
-//     }
+        return;
+    }
 
-//     event.respondWith(
-//         caches.open(CACHE_NAME).then(cache => {
-//             return cache.match(event.request).then(res => {
-//                 return res || fetch(event.request);
-//             });
-//         })
-//     );
-// });
+    event.respondWith(
+        caches.open(STATIC_CACHE).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
