71e3a9a8db381faa1bc0e691fb9dd6faf27827ac
dboren <danielboren@att.net>
2021-05-07 21:26:52 -0500

    renamed cache variables in preparation to rewrite sw event listeners

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,34 +9,65 @@ const FILES_TO_CACHE = [
 
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const STATIC_CACHE = 'static-cache-v2';
+const DATA_CACHE = 'data-cache-v1';
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(PRECACHE)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-            .then(self.skipWaiting())
-    );
-});
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches
+//             .open(PRECACHE)
+//             .then((cache) => cache.addAll(FILES_TO_CACHE))
+//             .then(self.skipWaiting())
+//     );
+// });
 
-self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
+// self.addEventListener('activate', (event) => {
+//     const currentCaches = [PRECACHE, RUNTIME];
+//     event.waitUntil(
+//         caches
+//             .keys()
+//             .then((cacheNames) => {
+//                 return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+//             })
+//             .then((cachesToDelete) => {
+//                 return Promise.all(
+//                     cachesToDelete.map((cacheToDelete) => {
+//                         return caches.delete(cacheToDelete);
+//                     })
 
-                );
-            })
-            .then(() => self.clients.claim())
-    );       
-});
+//                 );
+//             })
+//             .then(() => self.clients.claim())
+//     );       
+// });
+
+
+// self.addEventListener('fetch', function(event) {
+//     if (event.request.url.includes('/api/')) {
+//         event.respondWith(
+//             caches.open(DATA_CACHE_NAME).then(cache => {
+//                 return fetch(event.request)
+//                     .then(res => {
+//                         if (res.status === 200) {
+//                             cache.put(event.request.url, res.clone());
+//                         }
+
+//                         return res;
+//                     })
+//                     .catch(err => {
+//                         return cache.match(event.request);
+//                     });
+//             }).catch(err => console.log(err))      
+//         );
+
+//         return;
+//     }
+
+//     event.respondWith(
+//         caches.open(CACHE_NAME).then(cache => {
+//             return cache.match(event.request).then(res => {
+//                 return res || fetch(event.request);
+//             });
+//         })
+//     );
+// });
